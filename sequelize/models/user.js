a11f287327c1bff1489d83c92584b9ecee7cee15
mechaniccoder <yuseunghwan94@gmail.com>
2020-07-25 19:28:41 +0900

    Sequelize ORM with MySQL

---
+++ sequelize/models/user.js
@@ -0,0 +1,34 @@
+const sequelize = require("sequelize");
+
+module.exports = (sequelize, DataTypes) => {
+  return sequelize.define(
+    "user",
+    {
+      name: {
+        type: DataTypes.STRING(20),
+        allowNull: false,
+        unique: true,
+      },
+      age: {
+        type: DataTypes.INTEGER.UNSIGNED,
+        allowNull: false,
+      },
+      married: {
+        type: DataTypes.BOOLEAN,
+        allowNull: false,
+      },
+      comment: {
+        type: DataTypes.TEXT,
+        allowNull: true,
+      },
+      created_at: {
+        type: DataTypes.DATE,
+        allowNull: false,
+        defaultValue: sequelize.literal("now()"),
+      },
+    },
+    {
+      timestamps: false,
+    }
+  );
+};
