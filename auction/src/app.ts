4d7cf6ef9db20b2665ad90687d767615638c1199
mechaniccoder <yuseunghwan94@gmail.com>
2020-08-05 23:42:17 +0900

    Make express app

--- auction/src/app.ts
+++ auction/src/app.ts
@@ -3,14 +3,14 @@ import * as express from 'express';
 import * as session from 'express-session';
 import * as morgan from 'morgan';
 import * as cookieParser from 'cookie-parser';
-import * as flash from 'connect-flash';
+// import flash from 'connect-flash';
 import * as passport from 'passport';
 import * as path from 'path';
 import * as dotenv from 'dotenv';
 
 // 라우터
-// import indexRouter from './routes';
-// import authRouter from './routes/auth';
+import indexRouter from './routes';
+import authRouter from './routes/auth';
 
 // 모듈
 import db from './models';
@@ -27,7 +27,7 @@ passportConfig(passport);
 const sessionMiddleware = session({
   resave: false,
   saveUninitialized: false,
-  secret: process.env.COOKIE_SECRET,
+  secret: process.env.COOKIE_SECRET as string,
   cookie: {
     httpOnly: true,
     secure: false,
@@ -36,11 +36,39 @@ const sessionMiddleware = session({
 
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
+app.set('port', process.env.PORT || 8000);
 
-app.use('/', (req: express.Request, res: express.Response) => {
-  res.send('Hello World!');
+app.use(morgan('dev'));
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser(process.env.COOKIE_SECRET));
+app.use(sessionMiddleware);
+app.use(passport.initialize());
+app.use(passport.session());
+app.use(require('connect-flash')());
+
+app.use('/', indexRouter);
+app.use('/auth', authRouter);
+
+interface Error {
+  message?: string;
+  status?: number;
+}
+
+app.use((req, res, next) => {
+  const err: Error = new Error('Not Found');
+  err.status = 404;
+  next(err);
 });
 
-app.listen(3000, () => {
-  console.log('3000번 포트 서버 대기!');
+app.use((err: Error, req: express.Request, res: express.Response) => {
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+  res.status(err.status || 500);
+  res.render('error');
+});
+
+app.listen(app.get('port'), () => {
+  console.log(`${app.get('port')}번 포트 서버 대기!`);
 });
